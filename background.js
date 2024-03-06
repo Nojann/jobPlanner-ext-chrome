e40856df18665e9b040f97b3a0c751d097749502
Robin Couret <couret.robin@protonmail.com>
2024-03-06 14:28:56 +0100

    error handler

--- background.js
+++ background.js
@@ -1,39 +1,57 @@
 const url = "http://localhost:3000/"
 // const url = "http://www.localhost:3000/"
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getDOM') {
-    // Le contenu DOM est reçu depuis le script de contenu
-    const domContent = message.content;
 
-    // Ici, tu peux faire ce que tu veux avec le DOM récupéré, comme l'envoyer à un serveur, le traiter localement, etc.
-    console.log('DOM récupéré:', domContent);
+    const textContent = message.content;
 
-    chrome.cookies.get({ url: url, name: "signed_in" }, function(cookie) {
-      console.log(cookie);
+    chrome.cookies.get({ url: url, name: "signed_in" }, (cookie) => {
       if (cookie) {
-        const url_posts_api = `${url}/api/v1/posts`
-        const details = {
-          method: "POST",
-          headers: {
-          "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ content: domContent, token: cookie.value })
-        }
-        fetch(url_posts_api, details)
-        .then(response => response.json())
-        .then((data) => {
-          console.log(data);
-          if (data.status == 200) {
-            // chrome.runtime.sendMessage({ data.message })
-
-          } else {
-            chrome.runtime.sendMessage({ action: "nok", content: `${data.status}` })
-          }
-        })
+        console.log(cookie);
+        fetch_jobplanner_api(textContent, cookie);
       } else {
-      chrome.runtime.sendMessage({message: "cookie not found. User is not connected", action: "log_in" });
+        chrome.runtime.sendMessage({error: "Cookie not found. User is not connected", action: "log_in" });
       }
     });
   }
 });
+
+const fetch_jobplanner_api = (textContent, cookie) => {
+  const url_posts_api = `${url}/api/v1/posts`
+    const details = {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+      },
+      body: JSON.stringify({content: textContent, token: cookie.value })
+    }
+
+
+    fetch(url_posts_api, details)
+      .then(response => {
+        try {
+          if (response.status !== 201) {
+            throw new Error("Network response was not ok");
+          } else {
+            return response.json();
+          }
+        } catch (error) {
+          chrome.runtime.sendMessage({ action: "error_network", content: `Statut : ${response.status}` })
+          console.error('Error:', error);
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.message) {
+          chrome.runtime.sendMessage({ action: "create_ok", content: `` })
+        } else {
+          chrome.runtime.sendMessage({ action: "create_error", content: `` })
+        }
+      })
+      .catch ((error) => {
+        chrome.runtime.sendMessage({ action: "error_network", content: `${url_posts_api} is not found.` });
+        console.log('Error:', error);
+      })
+
+}
